54487ff47ae0eb0a51c5f650c4c25d2fb639c8bd
Oualid <oualids1007@gmail.com>
2017-03-29 13:45:31 +0200

    create_answer table bewerkt

--- database/migrations/2017_03_29_114254_create_answers_table.php
+++ database/migrations/2017_03_29_114254_create_answers_table.php
@@ -1,31 +1,36 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+// klasse voor antwoorden van vraag
 
 class CreateAnswersTable extends Migration
 {
     /**
-     * Run the migrations.
+     * 
      *
      * @return void
      */
+    //functie voor antwoorden
     public function up()
     {
         Schema::create('Answer', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('question_id');
+            $table->integer('survey_id');
+            $table->string('answer');
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * 
      *
      * @return void
      */
     public function down()
     {
-        Schema::dropIfExists('Answer');
+        Schema::drop('Answer');
     }
 }
