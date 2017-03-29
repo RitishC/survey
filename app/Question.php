17e5faca95589a6407917388e0e9e2993134e0a2
Oualid <oualids1007@gmail.com>
2017-03-29 13:31:51 +0200

    functie en klasse voor question model

--- app/Question.php
+++ app/Question.php
@@ -2,17 +2,35 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+// klasse voor aanmaken van vragen
 class CreateQuestionTable extends Migration
 {
     /**
-     * Run the migrations.
+     * 
      *
      * @return void
      */
-    public function ()
+    // functie voor toevoegen van vraag
+    public function up()
     {
-        
+        Schema::create('question', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('survey_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('title');
+            $table->string('question_type');
+            $table->string('option_name')->nullable();
+            $table->timestamps();
+        });
     }
 
-   
+    /**
+     * 
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('question');
+    }
+}
