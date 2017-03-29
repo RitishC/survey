f44a569d7d01168073f5c01095d66ced48a9b7c7
Ritish <ritishc@outlook.com>
2017-03-29 11:29:11 +0200

    Survey model aangepast

--- app/Survey.php
+++ app/Survey.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    //
+  protected $fillable = ['title', 'description', 'user_id'];
+  protected $dates = ['deleted_at'];
+  protected $table = 'survey';
+
+  public function questions() {
+    return $this->hasMany(Question::class);
+  }
+
+  public function user() {
+    return $this->belongsTo(User::class);
+  }
+  
+  public function answers() {
+    return $this->hasMany(Answer::class);
+  }
+
 }
