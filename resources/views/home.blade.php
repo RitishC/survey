7f8da4b06ff9c32a7acb4b77395d6659e1c1fdec
Oualid <oualids1007@gmail.com>
2017-03-29 09:42:40 +0200

    Home.blade.php toegevoegd

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layout')
+
+@section('content')
+    <ul class="collection with-header">
+        <li class="collection-header">
+            <h2 class="flow-text">Recente surveys
+                <span style="float:right;">{{ link_to_route('new.survey', 'Create new') }}
+            </span>
+            </h2>
+        </li>
+        @forelse ($surveys as $survey)
+            <li class="collection-item">
+                <div>
+                    {{ link_to_route('detail.survey', $survey->title, ['id'=>$survey->id])}}
+                    <a href="survey/view/{{ $survey->id }}" title="Vul survey in" class="secondary-content"><i class="material-icons">send</i></a>
+                    <a href="survey/{{ $survey->id }}" title="Verander survey" class="secondary-content"><i class="material-icons">edit</i></a>
+                    <a href="survey/answers/{{ $survey->id }}" title="Bekijk survey antwoorden" class="secondary-content"><i class="material-icons">textsms</i></a>
+                </div>
+            </li>
+        @empty
+            <p class="flow-text center-align">Nog geen surveys toegevoegd</p>
+        @endforelse
+    </ul>
+
+@stop
