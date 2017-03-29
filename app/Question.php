6118c492d86cfd8a0196a7e986eb8e0c208697d2
Ritish <ritishc@outlook.com>
2017-03-29 13:41:18 +0200

    Question model verbeterd

--- app/Question.php
+++ app/Question.php
@@ -1,36 +1,10 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
-// klasse voor aanmaken van vragen
-class CreateQuestionTable extends Migration
-{
-    /**
-     * 
-     *
-     * @return void
-     */
-    // functie voor toevoegen van vraag
-    public function up()
-    {
-        Schema::create('question', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('survey_id')->unsigned()->index();
-            $table->integer('user_id')->unsigned()->index();
-            $table->string('title');
-            $table->string('question_type');
-            $table->string('option_name')->nullable();
-            $table->timestamps();
-        });
-    }
+namespace App;
 
-    /**
-     * 
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('question');
-    }
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    //
 }
