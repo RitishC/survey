0c298f5b7b8fe6b990b9655479bfee841e10dbff
Oualid <oualids1007@gmail.com>
2017-03-29 10:25:02 +0200

    Controller toegevoegd dmv php artisan make:controller SurveyController

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -1,14 +1,13 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
 class SurveyController extends Controller
 {
-  public function home(Request $request) 
-  {
     # $surveys = Survey::get();
     # return view('home', compact('surveys'));
     return view('home');
   }
 }
-
-
-?>
