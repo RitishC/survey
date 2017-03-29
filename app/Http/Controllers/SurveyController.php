09e5ae893cffcd83ba96ab8bf8977c8071846c0c
Oualid <oualids1007@gmail.com>
2017-03-29 10:21:32 +0200

    SurveyController toegevoegd

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,14 @@
+<?php
+
+class SurveyController extends Controller
+{
+  public function home(Request $request) 
+  {
+    # $surveys = Survey::get();
+    # return view('home', compact('surveys'));
+    return view('home');
+  }
+}
+
+
+?>
