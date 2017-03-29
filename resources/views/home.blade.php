2245e323c9305fb2a1738108775d1b6716d8da6d
Ritish <ritishc@outlook.com>
2017-03-29 10:18:53 +0200

    tekst weer veranderd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,13 @@
             <li class="collection-item">
                 <div>
                     {{ link_to_route('detail.survey', $survey->title, ['id'=>$survey->id])}}
-                    <a href="survey/view/{{ $survey->id }}" title="Take Survey" class="secondary-content"><i class="material-icons">send</i></a>
-                    <a href="survey/{{ $survey->id }}" title="Edit Survey" class="secondary-content"><i class="material-icons">edit</i></a>
-                    <a href="survey/answers/{{ $survey->id }}" title="View Survey Answers" class="secondary-content"><i class="material-icons">textsms</i></a>
+                    <a href="survey/view/{{ $survey->id }}" title="Neem deel aan de survey !" class="secondary-content"><i class="material-icons">send</i></a>
+                    <a href="survey/{{ $survey->id }}" title="Verander Survey" class="secondary-content"><i class="material-icons">edit</i></a>
+                    <a href="survey/answers/{{ $survey->id }}" title="Bekijk survey antwoorden" class="secondary-content"><i class="material-icons">textsms</i></a>
                 </div>
             </li>
         @empty
-            <p class="flow-text center-align">Nothing to show</p>
+            <p class="flow-text center-align">Nog geen surveys aangemaakt !</p>
         @endforelse
     </ul>
 
