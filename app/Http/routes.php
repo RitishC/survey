47007253a4a0c1e482b50e3a129a5d115c40ff88
Ritish <ritishc@outlook.com>
2017-03-30 13:52:45 +0200

    route functie voor authorisatie toegevoegd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,24 +1,29 @@
 
 <?php
 
-Route::get('/', 'SurveyController@home');
+Route::group(['middleware' => 'auth'], function () {
 
-// Routes voor de survey
-Route::get('/survey/new', 'SurveyController@new_survey')->name('new.survey');
-Route::get('/survey/{survey}', 'SurveyController@detail_survey')->name('detail.survey');
-Route::get('/survey/view/{survey}', 'SurveyController@view_survey')->name('view.survey');
-Route::get('/survey/answers/{survey}', 'SurveyController@view_survey_answers')->name('view.survey.answers');
-Route::get('/survey/{survey}/delete', 'SurveyController@delete_survey')->name('delete.survey');
+	Route::get('/', 'SurveyController@home');
 
-Route::get('/survey/{survey}/edit', 'SurveyController@edit')->name('edit.survey');
-Route::patch('/survey/{survey}/update', 'SurveyController@update')->name('update.survey');
+	// Routes voor de survey
+	Route::get('/survey/new', 'SurveyController@new_survey')->name('new.survey');
+	Route::get('/survey/{survey}', 'SurveyController@detail_survey')->name('detail.survey');
+	Route::get('/survey/view/{survey}', 'SurveyController@view_survey')->name('view.survey');
+	Route::get('/survey/answers/{survey}', 'SurveyController@view_survey_answers')->name('view.survey.answers');
+	Route::get('/survey/{survey}/delete', 'SurveyController@delete_survey')->name('delete.survey');
 
-Route::post('/survey/view/{survey}/completed', 'AnswerController@store')->name('complete.survey');
-Route::post('/survey/create', 'SurveyController@create')->name('create.survey');
+	Route::get('/survey/{survey}/edit', 'SurveyController@edit')->name('edit.survey');
+	Route::patch('/survey/{survey}/update', 'SurveyController@update')->name('update.survey');
 
-// Routes voor de vragen
-Route::post('/survey/{survey}/questions', 'QuestionController@store')->name('store.question');
+	Route::post('/survey/view/{survey}/completed', 'AnswerController@store')->name('complete.survey');
+	Route::post('/survey/create', 'SurveyController@create')->name('create.survey');
 
-Route::get('/question/{question}/edit', 'QuestionController@edit')->name('edit.question');
-Route::patch('/question/{question}/update', 'QuestionController@update')->name('update.question');
+	// Routes voor de vragen
+	Route::post('/survey/{survey}/questions', 'QuestionController@store')->name('store.question');
+
+	Route::get('/question/{question}/edit', 'QuestionController@edit')->name('edit.question');
+	Route::patch('/question/{question}/update', 'QuestionController@update')->name('update.question');
+
+});
+
 Route::auth();
