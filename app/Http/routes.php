689acca352be6759faf8b371cdb15aa93c7407b1
Oualid <oualids1007@gmail.com>
2017-03-30 10:16:43 +0200

    survey controller in routes toegevoegd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,4 +2,5 @@
 
 Route::get('/', 'SurveyController@home')->name('home.page');
 Route::get('/', 'MyController@home')->name('home.page');
+Route::get('/survey/view/{survey}', 'SurveyController@view_survey')->name('view.survey');
 Route::auth();
