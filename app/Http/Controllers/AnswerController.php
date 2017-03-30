84a2ae1a3b4ccfc23ac3342435ccbe409c6a5d9b
Oualid <oualids1007@gmail.com>
2017-03-30 10:18:55 +0200

    answercontroller veranderd

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -1,10 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+//functie voor resultaten
+  public function store(Request $request, Survey $survey) 
+  {
+    
+    $arr = $request->except('_token');
+    foreach ($arr as $key => $value) {
+      $newAnswer = new Answer();
+      if (! is_array( $value )) {
+        $newValue = $value['answer'];
+      } else {
+        $newValue = json_encode($value['answer']);
+      }
+      $newAnswer->answer = $newValue;
+      $newAnswer->question_id = $key;
+      $newAnswer->user_id = Auth::id();
+      $newAnswer->survey_id = $survey->id;
+
+      $newAnswer->save();
+    };
+    return redirect()->action('SurveyController@view_survey_answers', [$survey->id]);
+  }
 }
