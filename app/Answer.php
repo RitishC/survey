5e2ec6d11022907a17e682c2e32d39751d01d084
Oualid <oualids1007@gmail.com>
2017-03-30 09:20:53 +0200

    Answer model gecodeerd

--- app/Answer.php
+++ app/Answer.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+// antwoorden klass aangemaakt
 class Answer extends Model
 {
-    //
+    protected $fillable = ['answer'];
+    protected $table = 'answer';
+
+    public function survey() {
+      return $this->belongsTo(Survey::class);
+    }
+
+    public function question() {
+      return $this->belongsTo(Question::class);
+    }
 }
