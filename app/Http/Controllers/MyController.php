14115032740ec115b7d7bf2f7c80d403be59a9e2
Ritish <ritishc@outlook.com>
2017-03-30 10:02:34 +0200

    MyController gecodeerd

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+
 class MyController extends Controller
 {
-    //
+    public home() {
+       return view('home');
+    }
 }
