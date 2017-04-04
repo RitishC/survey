bdb6f89ce000e7d17fa46fff6290a5173eb43cfb
Oualid <oualids1007@gmail.com>
2017-04-04 13:16:41 +0200

    surveycontroller gewijzigd

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,81 +2,101 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
+use App\ProtectedUrl;
 use App\Survey;
-use App\Answer;
+use Auth;
 use Illuminate\Http\Request;
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
-//klasse van surveycontroller incl functies 
+
 class SurveyController extends Controller
 {
-  //functie voor authoriseren
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
-//functie voor homepagina 
-  public function home(Request $request) 
-  {
-    $surveys = Survey::get();
-    return view('home', compact('surveys'));
-  }
+    public function home(Request $request)
+    {
+        $surveys = Survey::get();
+        return view('home', compact('surveys'));
+    }
 
-  // pagina laten zien om nieuwe survey aan te maken
-  public function new_survey() 
-  {
-    return view('survey.new');
-  }
-// functie voor aanmaken survey
-  public function create(Request $request, Survey $survey) 
-  {
-    $arr = $request->all();
-    // $request->all()['user_id'] = Auth::id();
-    $arr['user_id'] = Auth::id();
-    $surveyItem = $survey->create($arr);
-    return Redirect::to("/survey/{$surveyItem->id}");
-  }
+    # Show page to create new survey
+    public function new_survey()
+    {
+        return view('survey.new');
+    }
 
-  //details van survey pagina krijgen en mogelijkheid om vragen toe te voegen
-  public function detail_survey(Survey $survey) 
-  {
-    $survey->load('questions.user');
-    return view('survey.detail', compact('survey'));
-  }
-  
-// funnctie voor pagina van wijzigen van survey
-  public function edit(Survey $survey) 
-  {
-    return view('survey.edit', compact('survey'));
-  }
+    public function create(Request $request, Survey $survey)
+    {
+        $arr = $request->all();
+        // $request->all()['user_id'] = Auth::id();
+        $arr['user_id'] = Auth::id();
 
-  // wijzigen van survey
-  public function update(Request $request, Survey $survey) 
-  {
-    $survey->update($request->only(['title', 'description']));
-    return redirect()->action('SurveyController@detail_survey', [$survey->id]);
-  }
+        $surveyItem = $survey->create($arr);
 
-  // laten zien van survey
-  public function view_survey(Survey $survey) 
-  { 
-    $survey->option_name = unserialize($survey->option_name);
-    return view('survey.view', compact('survey'));
-  }
+        $protectedUrl = new ProtectedUrl();
+        $protectedUrl->survey_id = $surveyItem->id;
+        $protectedUrl->url = md5(time());
 
-  // weergeven van survey atwoorden
-  public function view_survey_answers(Survey $survey) 
-  {
-    $survey->load('user.questions.answers');
-    return view('answer.view', compact('survey'));
-  }
-  //verwijderen van survey
-  public function delete_survey(Survey $survey)
-  {
-    $survey->delete();
-    return redirect('');
-  }
+        $protectedUrl->save();
+
+        return Redirect::to("/survey/{$surveyItem->id}");
+    }
+
+    # retrieve detail page and add questions here
+    public function detail_survey(Survey $survey)
+    {
+        $data = [];
+        $survey->load('questions.user');
+
+        if ($survey->protected_urls->first() !== null) {
+            $data['url'] = $survey->protected_urls->first();
+        }
+        $data['survey'] = $survey;
+        return view('survey.detail', $data);
+    }
+
+
+    public function edit(Survey $survey)
+    {
+        return view('survey.edit', compact('survey'));
+    }
+
+    # edit survey
+    public function update(Request $request, Survey $survey)
+    {
+        $survey->update($request->only(['title', 'description']));
+        return redirect()->action('SurveyController@detail_survey', [$survey->id]);
+    }
+
+    # view survey publicly and complete survey
+    public function view_survey(Survey $survey)
+    {
+        $survey->option_name = unserialize($survey->option_name);
+        return view('survey.view', compact('survey'));
+    }
+
+    # view submitted answers from current logged in user
+    public function view_survey_answers(Survey $survey)
+    {
+        $survey->load('user.questions.answers');
+        // return view('survey.detail', compact('survey'));
+        // return $survey;
+        return view('answer.view', compact('survey'));
+    }
+
+    // TODO: Make sure user deleting survey
+    // has authority to
+    public function delete_survey(Survey $survey)
+    {
+        $this->middleware('auth');
+        $survey->delete();
+        return redirect('');
+    }
+
+    public function show_protected_survey($hash)
+    {
+        $survey = ProtectedUrl::where('url', $hash)->first()->survey;
+        $survey->load('questions.user');
+        $url = $survey->protected_urls->first();
+        return view('survey.detail', ['survey' => $survey, 'url' => $url]);
+
+    }
 
 }
