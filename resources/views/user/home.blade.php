69953ef791ee134a3e2bfad4bf3652e01df902e1
Anwar Elbo <anwar.elbouhdifi@gmail.com>
2017-06-25 19:16:10 +0200

    deleted arrow icon

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -9,7 +9,6 @@
             <li class="collection-item">
                 <div>
                     {{ link_to_route('detail.survey.user', $survey->title, ['id'=>$survey->id])}}
-                    <a href="survey/view/{{ $survey->id }}" title="Vul Survey in" class="secondary-content"><i class="material-icons">send</i></a>
                     <a href="survey/answers/{{ $survey->id }}" title="Surveyresultaten inzien" class="secondary-content"><i class="material-icons">textsms</i></a>
                 </div>
             </li>
